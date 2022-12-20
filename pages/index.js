48aea9dd0a931d7709432c4ca2a9bcf9e12d0765
yabamiyaba <ac3po6@gmail.com>
2022-12-20 12:54:38 +0900

    change index.js

--- pages/index.js
+++ pages/index.js
@@ -15,7 +15,7 @@ export default function Home() {
         </h1>
 
         <p className={styles.description}>
-          Get started by editing{' '}
+          OKOKOKOK{' '}
           <code className={styles.code}>pages/index.js</code>
         </p>
 
